abf5e723fb1b7cdf1acde5fb1ca040232a50d1a6
Colibri385 <colibri385@orange.fr>
2019-12-12 16:47:01 +0100

    second dépot

---
+++ apptest.js
@@ -0,0 +1,38 @@
+
+const mongoose = require('mongoose')
+const Article = require('./database/models/article')
+
+mongoose.connect('mongodb://localhost:27017/blog-test')
+
+
+
+
+
+
+/*
+Article.findById("5df217b7941d9e20644a223b", (error, articles) => {
+    console.log(error, articles);
+    
+})
+
+*/
+
+/*
+Article.find({
+    content: 'critique'
+}, (error, articles) => {
+    console.log(error, articles);
+    
+})
+*/
+
+/*
+Article.create({
+    title: "Spiderman",
+    intro: "test d'introduction",
+    content: "critique du film spiderman" }, 
+        (error, post) => {
+        console.log(error, post);
+}) 
+*/
+
